095c9510ecc724c303ec8c3f662ab7049b9df2fb
Nestor Iniguez <nextor.ivan@icloud.com>
2018-03-15 10:54:19 -0600

    Add node_modules
    Add folder node_modules

--- src/app/service/requisiciones.service.ts
+++ src/app/service/requisiciones.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { Http, Response, RequestOptions, Headers, HttpModule} from '@angular/Http';
+import { Observer } from 'rxjs/Observer';
+
+import { ApiConection} from './api-conection.service';
 
 @Injectable()
 export class RequisicionesService {
+  private urlRequiGet = ApiConection.ServiceUrl + ApiConection.damfo290;
+  constructor(private http: Http) { }
 
-  constructor() { }
+  
 
 }
