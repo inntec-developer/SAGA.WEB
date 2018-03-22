82d9f6259f8f6e9b63ea76af207939743ec81b5c
Nestor Iniguez <nextor.ivan@icloud.com>
2018-03-22 17:55:09 -0600

    Correccion Error Http

--- src/app/service/requisiciones/requisiciones.service.ts
+++ src/app/service/requisiciones/requisiciones.service.ts
@@ -1,5 +1,7 @@
+import { Http, Response, RequestOptions, Headers, HttpModule } from '@angular/http';
 import { Injectable } from '@angular/core';
-import { Http, Response, RequestOptions, Headers, HttpModule} from '@angular/Http';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/do';
@@ -8,7 +10,8 @@ import 'rxjs/add/operator/mergeMap';
 import 'rxjs/add/operator/toPromise';
 import 'rxjs/Rx';
 import 'rxjs/add/observable/throw';
-import { ApiConection } from '../index';
+
+import { ApiConection } from '../api-conection.service';
 
 @Injectable()
 export class RequisicionesService {
