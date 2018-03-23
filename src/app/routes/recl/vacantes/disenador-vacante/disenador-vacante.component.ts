2105b81ea200412cff188c74d6e40b2ccdef3110
David <emir265@hotmail.com>
2018-03-23 15:31:13 -0600

    diseñador de vacantes
    se agrego la parte del diseñador de componente para la configuracion de vacantes

---
+++ src/app/routes/recl/vacantes/disenador-vacante/disenador-vacante.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-disenador-vacante',
+  templateUrl: './disenador-vacante.component.html',
+  styleUrls: ['./disenador-vacante.component.scss']
+})
+export class DisenadorVacanteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
