e0818114ed58aba472d8426ba060780d6e74492f
Nestor Iniguez <nextor.ivan@icloud.com>
2018-03-23 15:34:45 -0600

    Alter rout, and add files

--- src/app/routes/vtas/requisiciones/components/requisicion-nueva/requisicion-nueva.component.ts
+++ src/app/routes/vtas/requisiciones/components/requisicion-nueva/requisicion-nueva.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, CanDeactivate, Router, } from '@angular/router';
+
+
+//Servicios
+import { CatalogosService } from '../../../../../service/index';
 
 @Component({
   selector: 'app-requisicion-nueva',
@@ -6,10 +11,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./requisicion-nueva.component.scss']
 })
 export class RequisicionNuevaComponent implements OnInit {
+  damfoId: string;
 
-  constructor() { }
+  constructor(
+    private serviceCatalogo: CatalogosService,
+    private _Router: Router,
+    private _Route: ActivatedRoute,
+  ) { }
 
   ngOnInit() {
+    var params = this._Route.params.subscribe( params =>{
+      this.damfoId = params["id"];
+    });
   }
-
 }
