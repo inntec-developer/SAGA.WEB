31a8f65f09cfeea793bbef66c6eb5b0160508de9
Nestor Iniguez <nextor.ivan@icloud.com>
2018-05-21 16:26:18 -0500

    Alter Requisicon nueva

--- src/app/routes/vtas/requisiciones/components/requisicion-nueva/requisicion-nueva.component.ts
+++ src/app/routes/vtas/requisiciones/components/requisicion-nueva/requisicion-nueva.component.ts
@@ -41,40 +41,6 @@ export class RequisicionNuevaComponent implements OnInit {
   ) { }
 
   ngOnInit() {
-    this.formCliente = this.fb.group({
-      nombrecomercial: [{value: '', disabled:true}],
-      razonSocial: [{value: '', disabled:true}],
-      rfc: [{value: '', disabled:true}],
-      giroEmpresa: [{value: '', disabled:true}],
-      actividadEmpresas: [{value: '', disabled:true}]
-    });
-
-    this.formRecl = this.fb.group({
-      tipo: [{value:'', disabled:true}],
-      clase: [{value:'', disabled:true}]
-    });
-
-    this.formContrato = this.fb.group({
-      tipoContrato: [{value:'', disabled:true}],
-      diasPrueba: [{value:'',disabled:true}]
-    });
-
-    this.formPerfil = this.fb.group({
-      vBtra:[{value: '', disabled:true}],
-      edadMinima:[{value: '', disabled:true}],
-      edadMaxima:[{value: '', disabled:true}],
-      genero:[{value: '', disabled:true}],
-      estadoCivil:[{value: '', disabled:true}],
-    });
-
-    this.formSueldo = this.fb.group({
-      diaCorte: [{value:'', disabled:true}],
-      tipoNomina: [{value:'', disabled:true}],
-      diaPago: [{value:'', disabled:true}],
-      periodoPago: [{value:'', disabled:true}],
-      especifique: [{value:'', disabled:true}],
-    });
-
     this.spinner.show();
       this._Route.params.subscribe(params => {
         if(params['IdDamfo'] != null && params['IdDireccion'] != null){
@@ -97,45 +63,6 @@ export class RequisicionNuevaComponent implements OnInit {
     this.serviceRequisiciones.createNewRequi(datas)
         .subscribe(data => {
           this.requisicionId = data;
-          this.getRequisicion(this.requisicionId);
         })
-  }
-
-getRequisicion(id){
-    this.serviceRequisiciones.getNewRequi(id)
-      .subscribe(data => {
-        this.formCliente.patchValue({
-          nombrecomercial: data.cliente.nombrecomercial,
-          razonSocial: data.cliente.razonSocial,
-          rfc: data.cliente.rfc,
-          giroEmpresa: data.cliente.giroEmpresas.giroEmpresa,
-          actividadEmpresas: data.cliente.actividadEmpresas.actividadEmpresa
-        });
-        this.formRecl.patchValue({
-          tipo: data.tipoReclutamiento.tipoReclutamiento,
-          clase: data.claseReclutamiento.clasesReclutamiento
-        });
-        this.formContrato.patchValue({
-          tipoContrato: data.contratoInicial.tipoContrato,
-          diasPrueba: data.tiempoContrato.tiempo
-        });
-        this.formPerfil.patchValue({
-          vBtra: data.vBtra,
-          edadMinima: data.edadaMinima,
-          edadMaxima: data.edadMaxima,
-          genero: data.genero.genero,
-          estadoCivil: data.estadoCivil.estadoCivil
-        });
-
-        this.formSueldo.patchValue({
-          diaCorte: data.diaCorte.diaSemana,
-          tipoNomina: data.tipoNomina.tipoDeNomina,
-          diaPago: data.diaPago.diaSemana,
-          periodoPago: data.periodoPago.periodoPago,
-          especifique: data.especifique
-        });
-        this.requisicion = data;
-        this.spinner.hide();
-      });
   }
 }
