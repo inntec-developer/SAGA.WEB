90b94ccf987caf918f77928b623449c3b4c16cdc
Pablovitch <34140568+Pablovitch@users.noreply.github.com>
2018-07-04 11:42:27 -0500

    uPDATE VACANTES DTL

--- src/app/routes/recl/candidatos/dt-candidatos/dialogcandidatos/dialogcandidatos.component.ts
+++ src/app/routes/recl/candidatos/dt-candidatos/dialogcandidatos/dialogcandidatos.component.ts
@@ -19,20 +19,6 @@ export class DialogcandidatosComponent implements OnInit {
 
   vacantesdtl: any[];
 
-  displayedColumnsVacantes = ['Vacante', 'Accion']
-  displayedColumns = ['Vacante', 'Estatus'];
-  public dataSourcev = new MatTableDataSource(<any>[]);
-  public dataSource = new MatTableDataSource(<any>[]);
-
-  // Objeto para el apartado de candidato.
-  public Apartado: FormGroup;
-
-  applyFilter(filterValue: string) {
-    filterValue = filterValue.trim(); // Remove whitespace
-    filterValue = filterValue.toLowerCase(); // MatTableDataSource defaults to lowercase matches
-    this.dataSourcev.filter = filterValue;
-  }
-
   constructor(public dialogRef: MatDialogRef<DialogcandidatosComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any, private service: CandidatosService) {
       this.service.getvacantesdtl(data)
