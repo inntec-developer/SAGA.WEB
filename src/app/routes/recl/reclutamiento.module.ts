9b815c5d13bdb302c129c113f3297ddd41676df0
Nestor Iniguez <nextor.ivan@icloud.com>
2018-07-05 19:04:27 -0500

    Add Dt-Vacantes-Reclutador

--- src/app/routes/recl/reclutamiento.module.ts
+++ src/app/routes/recl/reclutamiento.module.ts
@@ -1,71 +1,102 @@
 // Librerias necesarias para el modulo.
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { FormControl } from '@angular/forms';
-import { CommonModule } from '@angular/common';
-import { HttpModule } from '@angular/http';
-import { HttpClientModule } from '@angular/common/http';
-import { ColorPickerModule, ColorPickerService } from 'ngx-color-picker';
-import { SelectModule } from 'ng2-select';
-import { TextMaskModule } from 'angular2-text-mask';
-import { TagInputModule } from 'ngx-chips';
-import { CustomFormsModule } from 'ng2-validation';
-import { FileUploadModule } from 'ng2-file-upload';
-import { ImageCropperModule } from 'ng2-img-cropper';
-import { MatAutocompleteModule, MatButtonModule, MatButtonToggleModule,
-         MatCardModule, MatCheckboxModule, MatChipsModule,
-         MatDatepickerModule, MatDialogModule, MatDividerModule,
-         MatExpansionModule, MatGridListModule, MatIconModule,
-         MatInputModule, MatListModule, MatMenuModule,
-         MatNativeDateModule, MatPaginatorModule, MatProgressBarModule,
-         MatProgressSpinnerModule, MatRadioModule, MatRippleModule,
-         MatSelectModule, MatSidenavModule, MatSliderModule,
-         MatSlideToggleModule, MatSnackBarModule, MatSortModule,
-         MatStepperModule, MatTableModule, MatTabsModule,
-         MatToolbarModule, MatTooltipModule, MatOptionModule, MatPaginatorIntl
-       } from '@angular/material';
-import { startWith } from 'rxjs/operators/startWith';
-import { map } from 'rxjs/operators/map';
-import {PaginatorModule} from 'primeng/paginator';
 
-import { NgxSpinnerModule } from 'ngx-spinner';
-import { Damfo290Component } from './damfo290/damfo290.component';
-import { CandidatosComponent } from './candidatos/candidatos.component';
-import { VacantesComponent } from './vacantes/vacantes.component';
-import { BusquedaComponent } from './candidatos/busqueda/busqueda.component';
-import { PaisComponent } from './candidatos/busqueda/pais/pais.component';
-import { EstadoComponent } from './candidatos/busqueda/estado/estado.component';
-import { MunicipioComponent } from './candidatos/busqueda/municipio/municipio.component';
-import { ColoniaComponent } from './candidatos/busqueda/colonia/colonia.component';
-import { AreaExpComponent } from './candidatos/busqueda/area-exp/area-exp.component';
-import { CpComponent } from './candidatos/busqueda/cp/cp.component';
-import { GeneroComponent } from './candidatos/busqueda/genero/genero.component';
-import { EdadComponent } from './candidatos/busqueda/edad/edad.component';
-import { ReubicacionComponent } from './candidatos/busqueda/reubicacion/reubicacion.component';
-import { PcondiscapacidadComponent } from './candidatos/busqueda/pcondiscapacidad/pcondiscapacidad.component';
-import { TplicenciaComponent } from './candidatos/busqueda/tplicencia/tplicencia.component';
-import { VehpropioComponent } from './candidatos/busqueda/vehpropio/vehpropio.component';
-import { NivestudiosComponent } from './candidatos/busqueda/nivestudios/nivestudios.component';
-import { IdiomasComponent } from './candidatos/busqueda/idiomas/idiomas.component';
-import { PerfilComponent } from './candidatos/busqueda/perfil/perfil.component';
-import { DtVacantesComponent } from './candidatos/dt-vacantes/dt-vacantes.component';
-import { DtCandidatosComponent } from './candidatos/dt-candidatos/dt-candidatos.component';
-import { DialogcandidatosComponent } from './candidatos/dt-candidatos/dialogcandidatos/dialogcandidatos.component';
-import { DisenadorVacanteComponent } from './vacantes/disenador-vacante/disenador-vacante.component';
+import { ColorPickerModule, ColorPickerService } from 'ngx-color-picker';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {
+         MatAutocompleteModule,
+         MatButtonModule,
+         MatButtonToggleModule,
+         MatCardModule,
+         MatCheckboxModule,
+         MatChipsModule,
+         MatDatepickerModule,
+         MatDialogModule,
+         MatDividerModule,
+         MatExpansionModule,
+         MatGridListModule,
+         MatIconModule,
+         MatInputModule,
+         MatListModule,
+         MatMenuModule,
+         MatNativeDateModule,
+         MatOptionModule,
+         MatPaginatorIntl,
+         MatPaginatorModule,
+         MatProgressBarModule,
+         MatProgressSpinnerModule,
+         MatRadioModule,
+         MatRippleModule,
+         MatSelectModule,
+         MatSidenavModule,
+         MatSlideToggleModule,
+         MatSliderModule,
+         MatSnackBarModule,
+         MatSortModule,
+         MatStepperModule,
+         MatTableModule,
+         MatTabsModule,
+         MatToolbarModule,
+         MatTooltipModule
+} from '@angular/material';
+import { RouterModule, Routes } from '@angular/router';
 import {ToasterModule, ToasterService} from 'angular2-toaster';
 
-// Providers
-import { getSpanishPaginatorIntl } from '../../core/translator/config-paginator/config-paginator.component';
-import { VentaModule } from '../vtas/ventas.module';
+import { AreaExpComponent } from './candidatos/busqueda/area-exp/area-exp.component';
+import { BusquedaComponent } from './candidatos/busqueda/busqueda.component';
+import { CandidatosComponent } from './candidatos/candidatos.component';
+import { ColoniaComponent } from './candidatos/busqueda/colonia/colonia.component';
+import { CommonModule } from '@angular/common';
 import { ComponentsModule } from '../../components/components.module';
+import { CpComponent } from './candidatos/busqueda/cp/cp.component';
+import { CustomFormsModule } from 'ng2-validation';
+import { Damfo290Component } from './damfo290/damfo290.component';
+import { DialogcandidatosComponent } from './candidatos/dt-candidatos/dialogcandidatos/dialogcandidatos.component';
+import { DisenadorVacanteComponent } from './vacantes/disenador-vacante/disenador-vacante.component';
+import { DtCandidatosComponent } from './candidatos/dt-candidatos/dt-candidatos.component';
+import { DtVacantesComponent } from './candidatos/dt-vacantes/dt-vacantes.component';
+import { DtVacantesReclutadorComponent } from './vacantes/vacantes/dt-vacantes-reclutador/dt-vacantes-reclutador.component';
+import { EdadComponent } from './candidatos/busqueda/edad/edad.component';
+import { EstadoComponent } from './candidatos/busqueda/estado/estado.component';
+import { FileUploadModule } from 'ng2-file-upload';
+import { FormControl } from '@angular/forms';
+import { GeneroComponent } from './candidatos/busqueda/genero/genero.component';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpModule } from '@angular/http';
+import { IdiomasComponent } from './candidatos/busqueda/idiomas/idiomas.component';
+import { ImageCropperModule } from 'ng2-img-cropper';
+import { MunicipioComponent } from './candidatos/busqueda/municipio/municipio.component';
+import { NgModule } from '@angular/core';
+import { NgxSpinnerModule } from 'ngx-spinner';
+import { NivestudiosComponent } from './candidatos/busqueda/nivestudios/nivestudios.component';
+import {PaginatorModule} from 'primeng/paginator';
+import { PaisComponent } from './candidatos/busqueda/pais/pais.component';
+import { PcondiscapacidadComponent } from './candidatos/busqueda/pcondiscapacidad/pcondiscapacidad.component';
+import { PerfilComponent } from './candidatos/busqueda/perfil/perfil.component';
+import { ReubicacionComponent } from './candidatos/busqueda/reubicacion/reubicacion.component';
+import { SelectModule } from 'ng2-select';
+import { TagInputModule } from 'ngx-chips';
+import { TextMaskModule } from 'angular2-text-mask';
+import { TplicenciaComponent } from './candidatos/busqueda/tplicencia/tplicencia.component';
+import { VacantesComponent } from './vacantes/vacantes.component';
+import { VehpropioComponent } from './candidatos/busqueda/vehpropio/vehpropio.component';
+import { VentaModule } from '../vtas/ventas.module';
+import { getSpanishPaginatorIntl } from '../../core/translator/config-paginator/config-paginator.component';
+import { map } from 'rxjs/operators/map';
+import { startWith } from 'rxjs/operators/startWith';
+
+// Providers
+
+
+
+
 
 const routes: Routes = [
-    {path: '290', component: Damfo290Component},
-    {path: 'candidatos', component: CandidatosComponent},
-    {path: 'vacantes', component: VacantesComponent},
-    {path: 'disenador', component: DisenadorVacanteComponent},
-    { path: 'configuracionVacante/:Requi', component: DisenadorVacanteComponent}
+    { path: '290', component: Damfo290Component },
+    { path: 'candidatos', component: CandidatosComponent },
+    { path: 'vacantes', component: VacantesComponent },
+    { path: 'disenador', component: DisenadorVacanteComponent },
+    { path: 'configuracionVacante/:Requi', component: DisenadorVacanteComponent },
+    { path: 'vacantes', component: DtVacantesReclutadorComponent }
 ];
 
 @NgModule({
@@ -94,7 +125,7 @@ const routes: Routes = [
                     ReubicacionComponent, PcondiscapacidadComponent, TplicenciaComponent,
                     VehpropioComponent, NivestudiosComponent, IdiomasComponent,
                     PerfilComponent, DtVacantesComponent, DtCandidatosComponent,
-                     DialogcandidatosComponent, DisenadorVacanteComponent],
+                     DialogcandidatosComponent, DisenadorVacanteComponent, DtVacantesReclutadorComponent],
     entryComponents: [
        DialogcandidatosComponent],
     exports: [ RouterModule ]
---
+++ src/app/routes/recl/vacantes/vacantes/dt-vacantes-reclutador/dt-vacantes-reclutador.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dt-vacantes-reclutador',
+  templateUrl: './dt-vacantes-reclutador.component.html',
+  styleUrls: ['./dt-vacantes-reclutador.component.scss']
+})
+export class DtVacantesReclutadorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
